b793eec3e4ea2596ac7ce074df355ffcb878f1fc
NuclearMissile <torrific55@gmail.com>
2019-09-11 15:55:01 +0900

    lua decompiler

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"lua_go/binchunk"
+	"lua_go/vm"
 	"os"
 )
 
@@ -36,12 +37,14 @@ func printHeader(prototype *binchunk.Prototype) {
 	if prototype.IsVararg > 0 {
 		varargFlag = "+"
 	}
-	fmt.Printf("\n%s <%s:%d,%d> (%d instructions)\n", funcType,
-		prototype.Source, prototype.LineDefined, prototype.LastLineDefined, len(prototype.Code))
+	fmt.Printf("\n%s <%s:%d,%d>\n", funcType,
+		prototype.Source, prototype.LineDefined, prototype.LastLineDefined)
 	fmt.Printf("%d%s params, %d slots, %d upvalues, ",
 		prototype.NumParams, varargFlag, prototype.MaxStackSize, len(prototype.Upvalues))
 	fmt.Printf("%d locals, %d constants, %d functions\n", len(prototype.LocVars),
 		len(prototype.Constants), len(prototype.Protos))
+	fmt.Printf("Instructions (%d)\n", len(prototype.Code))
+	fmt.Println("\ti no.\tline no.name\t\toperands")
 }
 
 func printDetail(prototype *binchunk.Prototype) {
@@ -85,10 +88,52 @@ func constantToString(k interface{}) string {
 
 func printCode(prototype *binchunk.Prototype) {
 	for pc, c := range prototype.Code {
-		line := "-"
+		line := ""
 		if len(prototype.LineInfo) > 0 {
 			line = fmt.Sprintf("%d", prototype.LineInfo[pc])
 		}
-		fmt.Printf("\t%d\t[%s]\t0x%08X\n", pc+1, line, c)
+		// fmt.Printf("\t%d\t[%s]\t0x%08X\n", pc+1, line, c)
+		i := vm.Instruction(c)
+		fmt.Printf("\t%d\t[%s]\t%s \t", pc+1, line, i.OpName())
+		printOperands(i)
+		fmt.Printf("\n")
+	}
+}
+
+func printOperands(i vm.Instruction) {
+	switch i.OpMode() {
+	case vm.IABC:
+		a, b, c := i.ABC()
+		fmt.Printf("%d", a)
+		if i.BMode() != vm.OpArgN {
+			if b > 0xFF {
+				fmt.Printf(" %d", -1-b&0xFF)
+			} else {
+				fmt.Printf(" %d", b)
+			}
+		}
+		if i.CMode() != vm.OpArgN {
+			if c > 0xFF {
+				fmt.Printf(" %d", -1-c&0xFF)
+			} else {
+				fmt.Printf(" %d", c)
+			}
+		}
+	case vm.IABx:
+		a, bx := i.ABx()
+		fmt.Printf("%d", a)
+		if i.BMode() == vm.OpArgK {
+			fmt.Printf(" %d", -1-bx)
+		} else if i.BMode() == vm.OpArgU {
+			fmt.Printf(" %d", bx)
+		}
+	case vm.IAsBx:
+		a, sbx := i.AsBx()
+		fmt.Printf("%d %d", a, sbx)
+	case vm.IAx:
+		ax := i.Ax()
+		fmt.Printf("%d", -1-ax)
+	default:
+		panic("unknown instruction")
 	}
 }
--- vm/instruction.go
+++ vm/instruction.go
@@ -6,13 +6,13 @@ const MAXARG_sBx = MAXARG_Bx >> 1 // 262143 / 2 = 131071
 /*
  31       22       13       5    0
   +-------+^------+-^-----+-^-----
-  |b=9bits |c=9bits |a=8bits|op=6| ABC
+  |b=9bits |c=9bits |a=8bits|op=6| iABC
   +-------+^------+-^-----+-^-----
-  |    bx=18bits    |a=8bits|op=6| ABx
+  |    bx=18bits    |a=8bits|op=6| iABx
   +-------+^------+-^-----+-^-----
-  |   sbx=18bits    |a=8bits|op=6|
+  |   sbx=18bits    |a=8bits|op=6| iAsBx
   +-------+^------+-^-----+-^-----
-  |    ax=26bits            |op=6|
+  |    ax=26bits            |op=6| iAx
   +-------+^------+-^-----+-^-----
  31      23      15       7      0
 */
@@ -34,4 +34,29 @@ func (self Instruction) ABx() (a, bx int) {
 	a = int(self >> 6 & 0xFF)
 	bx = int(self >> 14)
 	return
+}
+
+func (self Instruction) AsBx() (a, sbx int) {
+	a, bx := self.ABx()
+	return a, bx - MAXARG_sBx
+}
+
+func (self Instruction) Ax() int {
+	return int(self >> 6)
+}
+
+func (self Instruction) OpName() string {
+	return opcodes[self.Opcode()].name
+}
+
+func (self Instruction) OpMode() byte {
+	return opcodes[self.Opcode()].opMode
+}
+
+func (self Instruction) BMode() byte {
+	return opcodes[self.Opcode()].argBMode
+}
+
+func (self Instruction) CMode() byte {
+	return opcodes[self.Opcode()].argCMode
 }

f1c283cf3b2ad2ac02b51c89118517d5036893de
NuclearMissile <torrific55@gmail.com>
2020-02-07 02:29:49 +0900

    update

--- src/compiler/emitter/eval_exp.go
+++ src/compiler/emitter/eval_exp.go
@@ -1,6 +1,10 @@
 package emitter
 
-import . "compiler/ast"
+import (
+	. "compiler/ast"
+	"compiler/lexer"
+	"vm"
+)
 
 // type of operands
 const (
@@ -32,7 +36,7 @@ func (fi *funcInfo) evalExp(node Exp, a, n int) {
 	case *FuncDefExp:
 		fi.evalFuncDefExp(exp, a)
 	case *TableCtorExp:
-		fi.evalTableConstructorExp(exp, a)
+		fi.evalTableCtorExp(exp, a)
 	case *UnopExp:
 		fi.evalUnopExp(exp, a)
 	case *BinopExp:
@@ -49,41 +53,190 @@ func (fi *funcInfo) evalExp(node Exp, a, n int) {
 }
 
 func (fi *funcInfo) evalFuncCallExp(exp *FuncCallExp, a int, n int) {
-
+	nArgs := fi.prepFuncCall(exp, a)
+	fi.emitCall(exp.Line, a, nArgs, n)
 }
 
 func (fi *funcInfo) evalTableAccessExp(exp *TableAccessExp, a int) {
+	regsBefore := fi.usedRegs
+	b, kindB := fi.expToOpArg(exp.PrefixExp, ArgRU)
+	c, _ := fi.expToOpArg(exp.KeyExp, ArgRK)
+	fi.usedRegs = regsBefore
 
+	if kindB == ArgUpval {
+		fi.emitGetTabUp(exp.LastLine, a, b, c)
+	} else {
+		fi.emitGetTable(exp.LastLine, a, b, c)
+	}
 }
 
 func (fi *funcInfo) evalNameExp(exp *NameExp, a int) {
-
+	if r := fi.slotOfLocalVar(exp.Name); r >= 0 {
+		fi.emitMove(exp.Line, a, r)
+	} else if idx := fi.indexOfUpval(exp.Name); idx >= 0 {
+		fi.emitGetUpval(exp.Line, a, idx)
+	} else {
+		fi.evalTableAccessExp(&TableAccessExp{
+			LastLine: exp.Line,
+			PrefixExp: &NameExp{
+				Line: exp.Line,
+				Name: "_ENV",
+			},
+			KeyExp: &StringExp{
+				Line: exp.Line,
+				Str:  exp.Name,
+			},
+		}, a)
+	}
 }
 
 func (fi *funcInfo) evalConcatExp(exp *ConcatExp, a int) {
-
+	for _, subExp := range exp.Exps {
+		a := fi.allocReg()
+		fi.evalExp(subExp, a, 1)
+	}
+	c := fi.usedRegs - 1
+	b := c - len(exp.Exps) + 1
+	fi.freeRegs(c - b + 1)
+	fi.emitABC(exp.Line, vm.OP_CONCAT, a, b, c)
 }
 
 func (fi *funcInfo) evalUnopExp(exp *UnopExp, a int) {
-
+	regsBefore := fi.usedRegs
+	b, _ := fi.expToOpArg(exp.Exp, ArgReg)
+	fi.emitUnaryOp(exp.Line, exp.Op, a, b)
+	fi.usedRegs = regsBefore
 }
 
 func (fi *funcInfo) evalBinopExp(exp *BinopExp, a int) {
-
+	switch exp.Op {
+	case lexer.TOKEN_OP_AND, lexer.TOKEN_OP_OR:
+		regsBefore := fi.usedRegs
+		b, _ := fi.expToOpArg(exp.Exp1, ArgReg)
+		fi.usedRegs = regsBefore
+		if exp.Op == lexer.TOKEN_OP_AND {
+			fi.emitTestSet(exp.Line, a, b, 0)
+		} else {
+			fi.emitTestSet(exp.Line, a, b, 1)
+		}
+		pcJmp := fi.emitJmp(exp.Line, 0, 0)
+		fi.usedRegs = regsBefore
+		fi.emitMove(exp.Line, a, b)
+		fi.setSBx(pcJmp, fi.pc()-pcJmp)
+	default:
+		regsBefore := fi.usedRegs
+		b, _ := fi.expToOpArg(exp.Exp1, ArgRK)
+		c, _ := fi.expToOpArg(exp.Exp2, ArgRK)
+		fi.emitBinaryOp(exp.Line, exp.Op, a, b, c)
+		fi.usedRegs = regsBefore
+	}
 }
 
-func (fi *funcInfo) evalTableConstructorExp(exp *TableCtorExp, a int) {
+func (fi *funcInfo) evalTableCtorExp(exp *TableCtorExp, a int) {
+	nArr := 0
+	for _, keyExp := range exp.KeyExps {
+		if keyExp == nil {
+			nArr++
+		}
+	}
+	nExps := len(exp.KeyExps)
+	multRet := nExps > 0 && isVarargOrFuncCall(exp.ValExps[nExps-1])
+	fi.emitNewTable(exp.Line, a, nArr, nExps-nArr)
+	arrIdx := 0
+	for i, keyExp := range exp.KeyExps {
+		valExp := exp.ValExps[i]
+		if keyExp == nil {
+			arrIdx++
+			tmp := fi.allocReg()
+			if i == nExps-1 && multRet {
+				fi.evalExp(valExp, tmp, -1)
+			} else {
+				fi.evalExp(valExp, tmp, 1)
+			}
+			if arrIdx%50 == 0 || arrIdx == nArr {
+				n := arrIdx % 50
+				if n == 0 {
+					n = 50
+				}
+				fi.freeRegs(n)
+				line := lastLineOf(valExp)
+				c := (arrIdx-1)/50 + 1
+				if i == nExps-1 && multRet {
+					fi.emitSetList(line, a, 0, c)
+				} else {
+					fi.emitSetList(line, a, n, c)
+				}
+			}
+			continue
+		}
 
+		b := fi.allocReg()
+		fi.evalExp(keyExp, b, 1)
+		c := fi.allocReg()
+		fi.evalExp(valExp, c, 1)
+		fi.freeRegs(2)
+
+		line := lastLineOf(valExp)
+		fi.emitSetTable(line, a, b, c)
+	}
 }
 
 func (fi *funcInfo) evalFuncDefExp(exp *FuncDefExp, a int) {
+	subFunc := newFuncInfo(fi, exp)
+	fi.subFuncs = append(fi.subFuncs, subFunc)
 
+	for _, param := range exp.Params {
+		subFunc.addLocVar(param, 0)
+	}
+
+	subFunc.evalBlock(exp.Block)
+	subFunc.exitScope(subFunc.pc() + 2)
+	subFunc.emitReturn(exp.LastLine, 0, 0)
+	fi.emitClosure(exp.LastLine, a, len(fi.subFuncs)-1)
 }
 
 func (fi *funcInfo) evalVarargExp(exp *VarargExp, a int, n int) {
-
+	if !fi.isVararg {
+		panic("cannot use '...' outside a vararg function")
+	}
+	fi.emitVararg(exp.Line, a, n)
 }
 
 func (fi *funcInfo) evalTailCallExp(exp *FuncCallExp, a int) {
+	nArgs := fi.prepFuncCall(exp, a)
+	fi.emitTailCall(exp.Line, a, nArgs)
+}
 
+func (fi *funcInfo) prepFuncCall(exp *FuncCallExp, a int) int {
+	nArgs := len(exp.Args)
+	lastArgIsVarargOrFuncCall := false
+
+	fi.evalExp(exp.PrefixExp, a, 1)
+	if exp.NameExp != nil {
+		fi.allocReg()
+		c, k := fi.expToOpArg(exp.NameExp, ArgRK)
+		fi.emitSelf(exp.Line, a, a, c)
+		if k == ArgReg {
+			fi.freeReg()
+		}
+	}
+
+	for i, arg := range exp.Args {
+		tmp := fi.allocReg()
+		if i == nArgs-1 && isVarargOrFuncCall(arg) {
+			lastArgIsVarargOrFuncCall = true
+			fi.evalExp(arg, tmp, -1)
+		} else {
+			fi.evalExp(arg, tmp, 1)
+		}
+	}
+	fi.freeRegs(nArgs)
+	if exp.NameExp != nil {
+		fi.freeReg()
+		nArgs++
+	}
+	if lastArgIsVarargOrFuncCall {
+		nArgs = -1
+	}
+	return nArgs
 }
--- src/compiler/emitter/eval_stat.go
+++ src/compiler/emitter/eval_stat.go
@@ -33,6 +33,46 @@ func (fi *funcInfo) evalStat(node Stat) {
 	}
 }
 
+func (fi *funcInfo) evalRetStat(retExps []Exp, lastLine int) {
+	n := len(retExps)
+	if n == 0 {
+		fi.emitReturn(lastLine, 0, 0)
+		return
+	}
+	if n == 1 {
+		if nameExp, ok := retExps[0].(*NameExp); ok {
+			if r := fi.slotOfLocalVar(nameExp.Name); r >= 0 {
+				fi.emitReturn(lastLine, r, 1)
+				return
+			}
+		}
+		if fcExp, ok := retExps[0].(*FuncCallExp); ok {
+			r := fi.allocReg()
+			fi.evalTailCallExp(fcExp, r)
+			fi.freeReg()
+			fi.emitReturn(lastLine, r, -1)
+			return
+		}
+	}
+
+	multRet := isVarargOrFuncCall(retExps[n-1])
+	for i, exp := range retExps {
+		r := fi.allocReg()
+		if i == n-1 && multRet {
+			fi.evalExp(exp, r, -1)
+		} else {
+			fi.evalExp(exp, r, 1)
+		}
+	}
+	fi.freeRegs(n)
+	a := fi.usedRegs
+	if multRet {
+		fi.emitReturn(lastLine, a, -1)
+	} else {
+		fi.emitReturn(lastLine, a, n)
+	}
+}
+
 func (fi *funcInfo) evalGotoStat(stat *GotoStat) {
 	pc := fi.emitJmp(stat.Line, 0, 0)
 	fi.addGoto(pc, fi.scopeLv, stat.Name)
